0a0f892ff598ad3dc7c1071eebc39a079f8dd484
“[amin <ma.abbasi1378@yahoo.com>
2023-01-15 16:54:37 +0330

    create utuls.py

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-import itertools
 import json
 import os
 import glob
 from pathlib import Path
-import statistics
 from collections import Counter
 
 from Project import Project
+from utils import list_to_json
 
 
-# def navigate_projects():
-
-
-def main():
+def create_metadata_of_projects(dataset_path):
     projects = []
     all_packages = []
-    dataset_path = 'dataset/'
     for project_path in [path for path in Path(dataset_path).iterdir() if path.is_dir()]:
         project_name = project_path.name
         main_paths = [path for path in glob.glob(f'{project_path}/*/**/', recursive=True) if
@@ -35,29 +30,26 @@ def main():
                               classes=classes, packages=packages, dataset_path=dataset_path)
             projects.append(project)
             all_packages = all_packages + packages
+
     list_to_json([project.get_dict() for project in projects], "metedata_projects")
     jaccards = (calculate_all_jaccard_similarity(projects))
     list_to_json(jaccards, "jaccards")
-    # exclusive_packages_percentage_result = calculate_exclusive_packages(all_packages, projects_data)
-    # threshold(exclusive_packages_percentage_result, 0.2)
+    exclusive_packages_percentage_result = calculate_percentage_of__exclusive_packages(all_packages, projects)
+    extract_outliers_by_threshold(exclusive_packages_percentage_result, 0.2)
 
 
-def list_to_json(data, name):
-    json_object = json.dumps(data)
-    open(f"{name}.json", "w").write(json_object)
 
 
-def calculate_exclusive_packages(all_packages, projects_data: dict):
+def calculate_percentage_of__exclusive_packages(all_packages, projects: dict):
     result = []
     counter = Counter(all_packages)
     exclusive_packages = [k for k, v in counter.items() if v == 1]
-    for project in projects_data.keys():
+    for project in projects:
         exclusives = 0
-        project_packages = projects_data[project]["packages"]
+        project_packages = project.packages
         for package in project_packages:
             if package in exclusive_packages:
                 exclusives = exclusives + 1
-        print((project, float(exclusives) / float(len(project_packages))))
         result.append((project, float(exclusives) / float(len(project_packages))))
     return result
 
@@ -84,13 +76,14 @@ def jaccard_similarity(project1_name, project1_packages, project2_name, project2
     return similarity
 
 
-def threshold(exclusive_packages_percentage_results, threshold_percentage):
+def extract_outliers_by_threshold(exclusive_packages_percentage_results, threshold_percentage):
     outlier_result = []
     for project, exclusive_packages_percentage in exclusive_packages_percentage_results:
         if exclusive_packages_percentage > threshold_percentage:
-            outlier_result.append(project)
-    print(outlier_result)
+            outlier_result.append(project.name)
 
 
 if __name__ == '__main__':
-    main()
+    dataset_path = 'dataset/'
+
+    create_metadata_of_projects(dataset_path)
